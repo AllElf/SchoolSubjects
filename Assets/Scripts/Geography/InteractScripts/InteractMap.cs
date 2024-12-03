d935b3cc4416a75f850827305fcebb90a048ea5f
AllElf <allelf26944@yandex.ru>
2024-12-03 19:39:02 +0500

    commit 4.7
    Интерактивная карта мира почти закончена

--- Assets/Scripts/Geography/InteractScripts/InteractMap.cs
+++ Assets/Scripts/Geography/InteractScripts/InteractMap.cs
@@ -1,21 +1,104 @@
 using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
 public class InteractMap : MonoBehaviour
 {
-    [SerializeField] Timer _timer;
     [SerializeField] Text _text;
+    [SerializeField] Text _textAnswer;
+    [SerializeField] Text _textResult;
+    [SerializeField] GameObject _panelResult;
     [SerializeField] float _count;
+
+
+    public Image _image;
+    [SerializeField] float _seconds;
+    public float fillAmounT = 1;
+    [SerializeField] float _speed = 0.1f;
     void Start()
     {
-        _timer = GameObject.FindObjectOfType<Timer>();
+        _seconds = _speed / _seconds;
+        StartCoroutine(Clock());
+
+        _panelResult.SetActive(false);
+        _count = 0f;
     }
 
+    private void Update()
+    {
+        if (_text != null)
+        {
+            _text.text = "Баллы: " + _count.ToString();
+            _textResult.text = "Результат:\n" + "Баллы: " + _count.ToString();
+        }
+        KeyButton();
+        
+    }
     
     public void CountPlus()
     {
-        _count++;
+        if (fillAmounT > 0f)
+        {
+            _count = _count + 0.5f;
+            _textAnswer.text = "Верно";
+        }
+    }
+    public void CountUnchanged()
+    {
+        if (fillAmounT > 0f)
+        {
+            _textAnswer.text = "Не верно";
+        }
+            
+    }
+    public void CountMinus()
+    {
+        if(fillAmounT > 0f)
+        {
+            if (_count > 0)
+            {
+                _count = _count - 0.5f;
+            }
+            else if (_count < 0)
+            {
+                _count = 0;
+            }
+        }
+        
+    }
+    void KeyButton()
+    {
+        if (Input.GetKeyUp(KeyCode.Alpha1))
+        {
+            CountPlus();
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha2))
+        {
+            CountUnchanged();
+        }
+        else if (Input.GetKeyUp(KeyCode.Alpha3))
+        {
+            CountMinus();
+        }
+        else if(Input.GetKeyUp(KeyCode.R))
+        {
+            SceneManager.LoadScene("GeographyWorldMap");
+        }
+    }
+    IEnumerator Clock()
+    {
+        while (true)
+        {
+            fillAmounT -= _seconds;
+            yield return new WaitForSeconds(_speed);
+            _image.fillAmount = fillAmounT;
+            if (fillAmounT <= 0)
+            {
+                //fillAmounT = 1;
+                _panelResult.SetActive(true);
+                break;
+            }
+        }
     }
 }
